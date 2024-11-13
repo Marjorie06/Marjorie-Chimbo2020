7e9564e5a4b5def4c74fe9082f3c90ddf55198f5
Marjorie06 <marjoriechal06@gmail.com>
2024-11-13 14:28:12 -0500

    primera practica

---
+++ MarjorieChimbo2020/src/main/java/ec/edu/espoch/marjoriechimbo2020/MarjorieChimbo2020.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.marjoriechimbo2020;
+
+/**
+ *
+ * @author SO-LAB1-PC13
+ */
+public class MarjorieChimbo2020 {
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(Systems.in);
+        double[] ventas= new double[7];
+        double totalVentas= 0;
+        double promedioVentas=0;
+        boolean salir= false;
+        System.out.println("Ingrese el monto de ventas para cada uno de los 7 días");
+        for (int i=0; i<7;i++){
+        do{
+        System.out.println("días"+ (i+1)+":");
+        ventas[i]= Scanner.nextDouble();
+        if (ventas)
+            
+       
+            
+                System.out.println("menu de opciones");
+            System.out.println("1. Calcular el total de ventas de la semana");
+            System.out.println("2. Calcular el día con ventas más cercanas al promedio");
+            System.out.println("3. Mostrar el día con la venta más alta y el dia con la venta mas baja");
+            System.out.println("4. Calcular el monto acumulado de ventas durante los primeros tres días y los últimos tres días");
+            System.out.println("5. Salir");
+            int opcion = scanner.extln();
+            
+            switch (opcion){
+                //caso 1
+                totalVentas
+            }
+                  
+            
+               
+        
+      
+        
+                
+        
+ 
+        
+        
+        
+                
+                
+        System.out.println("Hello World!");
+    }
+}
